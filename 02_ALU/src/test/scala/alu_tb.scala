3344d432de42f07bda2880439cbeea8e8a5242ae
Mustafa <mustafa.taleb@edu.rptu.de>
2025-12-21 16:49:13 +0100

    Failed test_AND_OR_XOR

--- 02_ALU/src/test/scala/alu_tb.scala
+++ 02_ALU/src/test/scala/alu_tb.scala
@@ -74,3 +74,76 @@ class ALUSUBTest extends AnyFlatSpec with ChiselScalatestTester {
     }
   }
 }
+class ALUANDTest extends AnyFlatSpec with ChiselScalatestTester {
+  "ALU_AND_Tester" should "test AND operation" in {
+    test(new ALU).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+      dut.clock.setTimeout(0)
+
+      dut.io.operation.poke(ALUOp.AND)
+
+      // Test 1 : AND with overlapping bits
+      dut.io.operandA.poke("hFF00FF00".U)
+      dut.io.operandB.poke("h0F0F0F0F".U)
+      dut.io.aluResult.expect("h0F000F00".U)
+
+      // Test 2 : AND with zero (masking)
+      dut.io.operandA.poke("h12345678".U)
+      dut.io.operandB.poke(0.U)
+      dut.io.aluResult.expect(0.U)
+
+      // Test 3 : AND with all ones (identity)
+      dut.io.operandA.poke("h89ABCDEF".U)
+      dut.io.operandB.poke("hFFFFFFFF".U)
+      dut.io.aluResult.expect("h89ABCDEF".U)
+    }
+  }
+}
+
+class ALUANDTest extends AnyFlatSpec with ChiselScalatestTester {
+  "ALU_OR_Tester" should "test OR operation" in {
+    test(new ALU).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+      dut.clock.setTimeout(0)
+
+      dut.io.operation.poke(ALUOp.OR)
+
+      // Test 1 : OR with overlapping bits
+      dut.io.operandA.poke("hF0F0F0F0".U)
+      dut.io.operandB.poke("h0F0F0F0F".U)
+      dut.io.aluResult.expect("hFFFFFFFF".U)
+
+      // Test 2 : OR with zero 
+      dut.io.operandA.poke("h12345678".U)
+      dut.io.operandB.poke(0.U)
+      dut.io.aluResult.expect("h12345678".U)
+
+      // Test 3 : OR with all ones
+      dut.io.operandA.poke("h00000000".U)
+      dut.io.operandB.poke("hFFFFFFFF".U)
+      dut.io.aluResult.expect("hFFFFFFFF".U)
+    }
+  }
+}
+class ALUXORTest extends AnyFlatSpec with ChiselScalatestTester {
+  "ALU_XOR_Tester" should "test XOR operation" in {
+    test(new ALU).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+      dut.clock.setTimeout(0)
+
+      dut.io.operation.poke(ALUOp.XOR)
+
+      // Test 1 : XOR different bits
+      dut.io.operandA.poke("hAAAA5555".U)
+      dut.io.operandB.poke("h5555AAAA".U)
+      dut.io.aluResult.expect("hFFFFFFFF".U)
+
+      // Test 2 : XOR with zero 
+      dut.io.operandA.poke("h12345678".U)
+      dut.io.operandB.poke(0.U)
+      dut.io.aluResult.expect("h12345678".U)
+
+      // Test 3 : XOR with itself
+      dut.io.operandA.poke("h12345678".U)
+      dut.io.operandB.poke("h12345678".U)
+      dut.io.aluResult.expect(0.U)
+    }
+  }
+}
